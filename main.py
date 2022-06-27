d840b18c6d1076672c706e210f310fdaa8f9c762
FrancescoCode1 <francesco.leotta@gmx.de>
2022-06-27 20:17:27 +0200

    Added File-Browser for setting save location. Default is still /download/

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ import os
 
 def downloader():
     music_list = []
-    sg.theme("Dark Teal 2")
+    sg.theme("black")
 
     # define the layout
     layout = [
-        [sg.Text("Input the Videolink:"), sg.InputText(key="-VIDEOLINK-", do_not_clear=False)],
-        [sg.Text("                            "), sg.Submit("Add", key="-ADD-", bind_return_key=True, button_color="red")],
-        [sg.Text("Format                 "), sg.Radio("wav", key="-WAV-", group_id="format"), sg.Radio("mp3", key="-MP3-", group_id="format"), sg.Radio("mp4", key="-MP4-", group_id="format")],
-        [sg.Text("                            "),sg.Listbox(values=[music_list], size=(45, 6), key="-LIST-")],
-        [sg.Submit("Convert", key="-CONVERT-", button_color="red"), sg.Cancel(button_color="red"), sg.Submit("Clear Last", key="-CLEAR_ALL-", button_color="red")]
+
+        [sg.Text("Input the Videolink:", background_color="#262626"), sg.InputText(key="-VIDEOLINK-", do_not_clear=False)],
+        [sg.Text("                            ", background_color="#262626"), sg.Submit("Add", key="-ADD-", bind_return_key=True, button_color="red")],
+        [sg.Text("Format                 ", background_color="#262626"), sg.Radio("wav", key="-WAV-", group_id="format", background_color="#262626"),
+         sg.Radio("mp3", key="-MP3-", group_id="format", background_color="#262626"), sg.Radio("mp4", key="-MP4-", group_id="format", background_color="#262626")],
+        [sg.Text("                            ", background_color="#262626"), sg.Listbox(values=[music_list], size=(45, 6), key="-LIST-")],
+        [sg.Text("Path                     ", background_color="#262626"),  sg.InputText(key="-FILEPATH-", default_text="\\download\\", do_not_clear=True),
+         sg.FolderBrowse("Save In", key="-SAVEIN-", target="-FILEPATH-")],
+        [sg.Submit("Convert", key="-CONVERT-", button_color="red"), sg.Cancel(button_color="red"), sg.Submit("Clear Last", key="-CLEAR_ALL-", button_color="red"), ]
     ]
 
     # create instance of the window
-    window = sg.Window("Youtube Download Tool", layout)
+    window = sg.Window("Youtube Download Tool", layout, background_color="#262626")
 
     # read events and values from window
     while True:
@@ -30,10 +34,16 @@ def downloader():
         wav = values["-WAV-"]
         mp3 = values["-MP3-"]
         mp4 = values["-MP4-"]
+        fname = values["-FILEPATH-"]
 
+        print(link, wav, mp4, mp3, fname)
         if event == "-ADD-":
             music_list.append(link)  # values["-VIDEOLINK-"]
             window["-LIST-"].update(music_list)
+
+        if event == "-SAVEIN-":
+             # values["-VIDEOLINK-"]
+            window["-FILEPATH-"].update(fname)
 
         if event == "-CLEAR_ALL-":
             music_list = music_list[:-1]
@@ -43,12 +53,12 @@ def downloader():
             print(music_list)
             if mp3:
                 for i in music_list:
-                    os.system(f"yt-dlp.exe -f ba -x --audio-format mp3 {i} -o \"\download\%(title)s.%(ext)s\"") # yt-dlp.exe -f ba -x --audio-format mp3 https://www.youtube.com/watch?v=da9PDzt53WA -o "%(id)s.%(ext)s"
+                    os.system(f"yt-dlp.exe -f ba -x --audio-format mp3 {i} -o \"{fname}\%(title)s.%(ext)s\"") # yt-dlp.exe -f ba -x --audio-format mp3 https://www.youtube.com/watch?v=da9PDzt53WA -o "%(id)s.%(ext)s"
                 sg.popup(f"DONE!")
 
             elif wav:
                 for i in music_list:
-                    os.system(f"yt-dlp.exe -f ba -x --audio-format wav {i} -o \"\download\%(title)s.%(ext)s\" ")
+                    os.system(f"yt-dlp.exe -f ba -x --audio-format wav {i} -o \"\{fname}\%(title)s.%(ext)s\" ")
                 sg.popup(f"DONE!")
 
             elif mp4:
